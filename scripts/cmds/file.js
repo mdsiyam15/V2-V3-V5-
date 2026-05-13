1af7be208f922d8162f60225f0ad2e2cd457753b
asma01302233-droid <asma01302233@gmail.com>
2026-05-13 12:54:45 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -5,7 +5,7 @@ module.exports = {
   config: {
     name: "filecmd",
     aliases: ["file"],
-    version: "1.0",
+    version: "1.1",
     author: "FARHAN-KHAN",
     countDown: 5,
     role: 2,
@@ -16,11 +16,55 @@ module.exports = {
   },
 
   onStart: async function ({ args, message }) {
+
+    // 🔐 ADMIN CHECK
+    if (message.role !== 2) {
+
+      const replies = [
+`
+👑𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+😾 কিরে ফাইল কি  তোর? 😼
+🙄 বাপে বানাইছে আবালচোদা 🖕
+          👑 𝆠፝𝐍𝐈𝐉𝐇𝐔𝐌-𝐁𝐎𝐓 👑
+`,
+
+`
+👑𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+😼 এই ফাইল কমান্ড টা তোর জন্য না 
+🫵 শুধু  তোর বাপ 😾 𝆠፝𝐒𝐈𝐘𝐀𝐌-এর জন্য 🪬
+          👑 𝆠፝𝐍𝐈𝐉𝐇𝐔𝐌-𝐁𝐎𝐓 👑
+`,
+
+`
+👑𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+😾  পুটকিমারা দে 🖕গা 🥵
+🥵 এইটা  তোর বাপের জন্য 😵
+          👑 𝆠፝𝐍𝐈𝐉𝐇𝐔𝐌-𝐁𝐎𝐓 👑
+`,
+
+`
+👑𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 👑
+😼 মাদারচোদ তোর বোনরে বিয়া দিবি🙅‍♂️
+🔐  তাহলে ফাইল দিব আগে চোদবো🖕
+          👑 𝆠፝𝐍𝐈𝐉𝐇𝐔𝐌-𝐁𝐎𝐓 👑
+`
+      ];
+
+      const randomReply = replies[Math.floor(Math.random() * replies.length)];
+      return message.reply(randomReply);
+    }
+
+    // 📌 COMMAND NAME CHECK
     const cmdName = args[0];
-    if (!cmdName) return message.reply("❌ | Please provide the command name.\nExample: filecmd fluxsnell");
+    if (!cmdName) {
+      return message.reply("❌ | Please provide the command name.\nExample: filecmd fluxsnell");
+    }
 
     const cmdPath = path.join(__dirname, `${cmdName}.js`);
-    if (!fs.existsSync(cmdPath)) return message.reply(`❌ | Command "${cmdName}" not found in this folder.`);
+
+    if (!fs.existsSync(cmdPath)) {
+      return message.reply(`❌ | Command "${cmdName}" not found in this folder.`);
+    }
 
     try {
       const code = fs.readFileSync(cmdPath, "utf8");
@@ -32,6 +76,7 @@ module.exports = {
       return message.reply({
         body: `📄 | Source code of "${cmdName}.js":\n\n${code}`
       });
+
     } catch (err) {
       console.error(err);
       return message.reply("❌ | Error reading the file.");
