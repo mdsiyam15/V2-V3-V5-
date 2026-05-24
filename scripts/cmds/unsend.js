7ef6e3e43f02ada72b004cf55bf39698969fdf36
RJsiyam-crypto <mdsiyam01608043961@gmail.com>
2026-05-25 03:31:27 +0600

    Update unsend.js

--- scripts/cmds/unsend.js
+++ scripts/cmds/unsend.js
@@ -1,9 +1,19 @@
+// 😼 Author: 𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍 😼
+// ⚠️ নাম চেঞ্জ করলে ফাইল নষ্ট হয়ে যাবে ভাই 😾
+
+const AUTHOR_LOCK = "𝆠፝𝐒𝐈𝐘𝐀𝐌-𝐇𝐀𝐒𝐀𝐍";
+const VISIBLE_AUTHOR = "MR_FARHAN";
+
+if (VISIBLE_AUTHOR !== "MR_FARHAN") {
+	throw new Error("Author Locked!");
+}
+
 module.exports = {
 	config: {
 		name: "unsend",
 		aliases: ["s", "siyam", "uns"],
 		version: "1.2",
-		author: "MR_FARHAN",
+		author: VISIBLE_AUTHOR,
 		countDown: 5,
 		role: 0,
 		description: {
@@ -19,16 +29,30 @@ module.exports = {
 
 	langs: {
 		vi: {
-			syntaxError: "Vui lòng reply tin nhắn muốn gỡ của bot"
+			syntaxError: "Vui lòng reply tin nhắn muốn gỡ của bot",
+			cantUnsend: "Không thể gỡ tin nhắn này"
 		},
 		en: {
-			syntaxError: "Please reply the message you want to unsend"
+			syntaxError: "Please reply the message you want to unsend",
+			cantUnsend: "Cannot unsend this message"
 		}
 	},
 
 	onStart: async function ({ message, event, api, getLang }) {
-		if (!event.messageReply || event.messageReply.senderID != api.getCurrentUserID())
+
+		if (!event.messageReply)
 			return message.reply(getLang("syntaxError"));
-		message.unsend(event.messageReply.messageID);
+
+		const botID = api.getCurrentUserID();
+
+		if (event.messageReply.senderID != botID)
+			return message.reply(getLang("cantUnsend"));
+
+		try {
+			await api.unsendMessage(event.messageReply.messageID);
+		}
+		catch (e) {
+			return message.reply(getLang("cantUnsend"));
+		}
 	}
 };
